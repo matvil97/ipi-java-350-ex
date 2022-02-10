e190d8127bc9eabae15b5627a8b1642d57f088bd
matvil97 <84513556+matvil97@users.noreply.github.com>
2022-02-10 20:24:43 +0100

    Eval rendu

--- src/main/java/com/ipiecoles/java/java350/exception/EmployeException.java
+++
@@ -1,7 +0,0 @@
-package com.ipiecoles.java.java350.exception;
-
-public class EmployeException extends Exception {
-    public EmployeException(String s) {
-        super(s);
-    }
-}
--- src/main/java/com/ipiecoles/java/java350/model/Poste.java
+++
@@ -1,7 +0,0 @@
-package com.ipiecoles.java.java350.model;
-
-public enum Poste {
-    TECHNICIEN,
-    MANAGER,
-    COMMERCIAL
-}
